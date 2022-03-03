555399b67a2e93b860a71914a3cd618390547b8b
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2022-03-03 19:13:30 +0530

    .

---
+++ 03-Methods and Functions/Functions-test.py
@@ -0,0 +1,34 @@
+#print("Hello")
+def evencheck(numlist):
+    for i in numlist:
+        #print("hello")
+        #print(f"Checking {i}")
+        if i % 2 == 0 :
+            print(f"this is even {i}")
+            return True
+        else:
+            pass
+    return False
+
+
+    #print("hello")
+
+#one=[3,5,23,20,48,46]
+one=[3,5,23,10]
+
+
+result = evencheck(one)
+print(f"the value of result is {result}")
+
+
+
+
+'''
+*/
+
+def returncheck(input):
+    print("hello")
+    #return "hello"
+returncheck("ello")
+
+'''
