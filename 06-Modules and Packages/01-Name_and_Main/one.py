403899f339c8cf530b4ee0e80a392b8630e4a12a
abdulazeezt <63993618+abdulazeezt@users.noreply.github.com>
2023-08-15 14:27:19 +0530

    .
    .

--- 06-Modules and Packages/01-Name_and_Main/one.py
+++ 06-Modules and Packages/01-Name_and_Main/one.py
@@ -5,5 +5,7 @@ def one_func():
 
 if __name__ == "__main__":
     print("This is been run from one.py directly")
+else:
+    print(" This is one.py : THis is been running through another script    ")
 
 ##########################################################
---
+++ 06-Modules and Packages/01-Name_and_Main/two.py
@@ -0,0 +1,14 @@
+from one import one_func
+
+one_func()
+
+################# One.py #################################
+def two_func():
+    print(" I am from two_func")
+
+if __name__ == "__main__":
+    print("This is been run from two.py directly")
+else:
+    print(" this is two.py : THis is been running through another script    ")
+
+##########################################################
